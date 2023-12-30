f3137567089466f4a25e7c0cccda9814888afdb1
ImaneDKHISSI <imane.dk99@gmail.com>
2023-12-30 15:13:50 +0100

    Ajout des appels de la fonction de Multiplication

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -3,5 +3,9 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+		System.out.println(Calculatrice.multiplication(2,3));
+		System.out.println(Calculatrice.multiplication(5,7));
+		System.out.println(Calculatrice.multiplication(9,2));
+		System.out.println(Calculatrice.multiplication(11,3));
 	}
 }
